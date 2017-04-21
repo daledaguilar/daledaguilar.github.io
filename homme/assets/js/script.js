59eef0f5c022bebd6c2b9797ca193e902407fc78
daledaguilar <daledaguilar@berkeley.edu>
2017-04-20 18:01:20 -0700

    New website

---
+++ homme/assets/js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+
+  // QUESTION 6
+
+  //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
+  $("#sidebar-button").click(function() {
+    // Your code here
+
+    if ($(".sidebar-container").hasClass("sidebar-active")) {
+      $("body").removeClass("no-scroll");
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active")
+    } else {
+      $("#sidebar-button").addClass("button-active");
+      $(".sidebar-container").addClass("sidebar-active");
+      $(".page-wrapper").addClass("wrapper-active");
+      setTimeout(function() {
+    $('body').addClass('no-scroll');
+    }, 300);
+    }
+  })
+
+  $(".page-wrapper").click(function(){
+
+  })
+
+  // QUESTION 7
+$(".page-wrapper").click(function(){
+   if ($(".sidebar-container").hasClass("sidebar-active")) {
+    $("body").removeClass("no-scroll");
+    $("#sidebar-button").removeClass("button-active");
+    $(".sidebar-container").removeClass("sidebar-active");
+    $(".page-wrapper").removeClass("wrapper-active")
+    setTimeout(function() {
+  $('body').addClass('no-scroll');
+  }, 300);
+  }
+
+})
+  //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
+
+  // QUESTION 8
+  $("#carousel-next").click(function() {
+  var marginleft = parseInt($("#carousel").css("margin-left").replace("px", ""))
+  if (marginleft == -3840) {
+    return false;
+  }
+  else {
+    $("#carousel").css("margin-left", marginleft - 960);
+  }
+});
+
+
+
+  //Implement the "slide to right" when the user clicks on #carousel-prev here
+  $("#carousel-prev").click(function() {
+    var marginleft = parseInt($("#carousel").css("margin-left").replace("px", ""))
+        if (marginleft == 0) {
+        return false;
+    }
+    else {
+      $("#carousel").css("margin-left", marginleft + 960);
+    }
+  });
+
+
+
+  //THIS IS NOT A REQUIRED QUESTION
+  // EXTRA FOR EXPERTS
+
+  // Implement a "smooth scroll" when the user clicks on the sidebar links here
+
+});
